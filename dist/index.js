7e3a1b017f1e922fbd396ef69667b2c77836652c
Juho Pyo <juhopyo@Juhos-MacBook-Pro.local>
2022-01-27 16:57:36 -0800

    Thursday, January 27, 2022 setup

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const main = async () => {
+    const orm = await core_1.MikroORM.init();
+};
+console.log("hello THere");
+//# sourceMappingURL=index.js.map
